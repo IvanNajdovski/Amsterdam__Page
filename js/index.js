9c1d0d6373fed6d2ba7c47aa04c4900e2ef823e1
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-09-25 11:40:23 +0200

    added some pictures and content

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
+// function scrollWindow(){
+//
+//     var newScrollTop = $( window ).scrollTop();
+//     var height = $( window ).height();
+//     console.log(newScrollTop);
+//     if( currentScrollTop > newScrollTop ){
+//         var newPosition = currentScrollTop - height;
+//
+//         //Remove our scroll event during animation
+//         $(window).off();
+//
+//         $( "html, body" ).animate({ scrollTop: newPosition }, 500, function(){
+//             currentScrollTop = $( window ).scrollTop();
+//
+//             //Rebind event after animation complete
+//             $( window ).on("scroll", scrollWindow );
+//
+//         });
+//     }
+//     else if( currentScrollTop < newScrollTop ){
+//
+//         //Remove our scroll event during animation
+//         $(window).off();
+//
+//         var newPosition = currentScrollTop + height;
+//         $( "html, body" ).animate({ scrollTop: newPosition }, 500, function(){
+//             currentScrollTop = $( window ).scrollTop();
+//
+//             //Rebind event after animation complete
+//             $( window ).on("scroll", scrollWindow );
+//
+//         });
+//     }
+//
+//     //Update scrollTop
+//     currentScrollTop = newScrollTop();
+//
+// }
+
+
 $(document).ready(function () {
 
     $(".menu__popup__item").on("mouseleave", function () {
@@ -28,51 +68,108 @@ $(document).ready(function () {
 
     var counterUp = 0;
 
+    // $(window).on("mousewheel DOMMouseScroll", function (e) {
+    //     if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    //         if (e.originalEvent.detail > 0) {
+    //             if (Math.abs(counterUp) < (width * 10) - 100) {
+    //                 counterUp = counterUp - 100;
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             }
+    //             else {
+    //                 counterUp = -width * 10
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             }
+    //
+    //
+    //         }
+    //         else {
+    //             if (counterUp < 0) {
+    //                 counterUp = counterUp + 100;
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             } else {
+    //                 counterUp = 0
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
+    //             }
+    //
+    //         }
+    //     } else {
+    //         if (e.originalEvent.wheelDelta < 0) {
+    //             if (Math.abs(counterUp) < (width * 10) - 100) {
+    //                 counterUp = counterUp - 100;
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             }
+    //             else {
+    //                 counterUp = -width * 10
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             }
+    //         } else {
+    //             if (counterUp < 0) {
+    //                 counterUp = counterUp + 100;
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+    //             } else {
+    //                 counterUp = 0
+    //                 $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
+    //             }
+    //         }
+    //     }
+    // });
+
     $(window).on("mousewheel DOMMouseScroll", function (e) {
-        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-            if (e.originalEvent.detail > 0) {
-                if (Math.abs(counterUp) < (width * 10) - 100) {
-                    counterUp = counterUp - 100;
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+        console.log($("body,html").outerHeight())
+        console.log($(window).outerHeight())
+
+        if ($("body,html").outerHeight() -100 > $(window).outerHeight()) {
+
+        } else {
+            if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+                if (e.originalEvent.detail > 0) {
+                    if (Math.abs(counterUp) < (width * 10) - 100) {
+                        counterUp = counterUp - 100;
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    }
+                    else {
+                        counterUp = -width * 10
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    }
+
+
                 }
                 else {
-                    counterUp = -width * 10
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    if (counterUp < 0) {
+                        counterUp = counterUp + 100;
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    } else {
+                        counterUp = 0
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
+                    }
+
                 }
-
-
             }
             else {
-                if (counterUp < 0) {
-                    counterUp = counterUp + 100;
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                if (e.originalEvent.wheelDelta < 0) {
+                    if (Math.abs(counterUp) < (width * 10) - 100) {
+                        counterUp = counterUp - 100;
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    }
+                    else {
+                        counterUp = -width * 10
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    }
                 } else {
-                    counterUp = 0
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
-                }
-
-            }
-        } else {
-            if (e.originalEvent.wheelDelta < 0) {
-                if (Math.abs(counterUp) < (width * 10) - 100) {
-                    counterUp = counterUp - 100;
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
-                }
-                else {
-                    counterUp = -width * 10
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
-                }
-            } else {
-                if (counterUp < 0) {
-                    counterUp = counterUp + 100;
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
-                } else {
-                    counterUp = 0
-                    $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
+                    if (counterUp < 0) {
+                        counterUp = counterUp + 100;
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,${counterUp}, 0)`);
+                    } else {
+                        counterUp = 0
+                        $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
+                    }
                 }
             }
         }
     });
+
+
+
 
     $(".arrow__right").on("click", function () {
         if (Math.abs(counterUp) < (width * 10) - $(window).width()) {
@@ -92,7 +189,6 @@ $(document).ready(function () {
             $(".main__content__box-flex").css("transform", `matrix(1, 0, 0, 1,0, 0)`);
         }
     });
-
 
 
     setTimeout(function () {
@@ -115,6 +211,54 @@ $(document).ready(function () {
         $(".main__content__build-header").addClass("active")
 
     }, 4000);
+
+    var flexWidth = $(".main__content__box-flex").children("a");
+    var fullWidth = 0
+    for (let item of flexWidth) {
+
+        fullWidth = fullWidth + $(item).outerWidth()
+    }
+
+    $("a").on("click", function (e) {
+
+        itemWidth = 0;
+        e.preventDefault()
+        $(".content__bottom").toggleClass("active")
+        var index = $(this).index()
+        for (var i = 0; i < index; i++) {
+            itemWidth = itemWidth + $(this).outerWidth()
+
+        }
+        $('.main__content__box-flex').children("a").not(this).each(function () {
+            $(this).removeClass('active');
+
+        });
+
+        if ($(this).hasClass("active")) {
+            if (Math.abs(counterUp) > fullWidth - $(window).width()) {
+                counterUp = -fullWidth + $(window).width()
+                $(".main__content__box-flex").css("transform", `matrix(1,0,0,1,${counterUp},0)`);
+            }
+
+        } else {
+            counterUp = -itemWidth
+            $(".main__content__box-flex").css("transform", `matrix(1,0,0,1,${counterUp},0)`);
+        }
+        $(this).toggleClass("active")
+    })
+    // $(window).scroll(function(e){
+    //     parallaxScroll();
+    // });
+
+    // function parallaxScroll(){
+    //     var scrolled = $(window).scrollTop();
+    //     var winight = $(window).height()
+    //     $('#parallax-bg-1').css('translateY',`(${-(scrolled*.25)}px)`);
+    //
+    //     $('#parallax-bg-3').css('translateY',`(${winight-150-(scrolled*.75)}px)`);
+    //
+    // }
+
 
 
 });
